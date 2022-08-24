3137bfb3fcb0bc56c9332b61db345b2051204dbc
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-08-24 09:51:15 +0100

    [Fix]: Fixed an error that bety has been flaged as an issue

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_uncle - Function that finds the uncle of a node
- * @tree: Pointer to the node to find the uncle
+ * @node: Pointer to the node to find the uncle
  * Return: Null or pointer to the uncle node
  */
 
@@ -27,4 +27,3 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (a->left);
 	return (a->right);
 }
-		
