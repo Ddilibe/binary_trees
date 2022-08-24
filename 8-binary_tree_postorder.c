c5d2530e9baef8ba5f81be2793d2c2cd70c0c47f
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-08-24 05:08:30 +0100

    [Fix]: Fixed an error that had to do with the binary tree post order function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,14 +10,14 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL)
-                return ;
-        if (!func)
-                return ;
+	if (tree == NULL)
+		return;
+	if (!func)
+		return;
 
-        if (tree->left)
-                binary_tree_postorder(tree->left, func);
-        if (tree->right)
-                binary_tree_postorder(tree->right, func);
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
